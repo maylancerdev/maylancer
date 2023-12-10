743b7a45ce87a7d685634324076a22cb70367abb
olakunlevpn <olakunlevpn@live.com>
2023-12-10 18:22:50 +0200

    first commit

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -42,7 +42,8 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     {
         Gate::define('viewNova', function ($user) {
             return in_array($user->email, [
-                //
+                'olakunle@maylancer.org',
+                'olakunleips@gmail.com',
             ]);
         });
     }
