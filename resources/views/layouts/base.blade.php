47db09da94ea37b7ca019ba060d44cc334dce22a
olakunlevpn <olakunlevpn@live.com>
2024-01-09 21:42:56 +0200

    wip

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -6,8 +6,8 @@
     <meta name="application-name" content="{{ config('app.name') }}">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>{{ config('app.name') }}</title>
+    @include('layouts.meta')
+    <title>{{ $title ?? 'Exceptional websites & web application Development' }} | {{ config('app.name') }}</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
